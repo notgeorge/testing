1dafbc7bdae1a346c3655f63257975cc6a24fa9a
george <george@roguegenius.com>
2015-12-31 18:49:06 +0000

    first post

---
+++ app.py
@@ -0,0 +1,17 @@
+
+
+class test:
+	def test_function(self):
+		x=1
+		self.test_subfunction()
+		self.test_n()
+		self.test_subfunction() 
+
+	def test_subfunction(self):
+		y=2	
+	
+	def test_n(self):
+		z=3
+		self.test_subfunction()
+
+		
